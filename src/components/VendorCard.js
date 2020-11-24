a9b68fd109e7a6f1a03b26941030aa62641f226f
jsans24 <jsans24@gmail.com>
2020-11-24 09:16:44 -0800

    JS7 - created vendor list page
    Signed-off-by: jsans24 <jsans24@gmail.com>

--- src/components/VendorCard.js
+++ src/components/VendorCard.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const VendorCard = (props) => {
+  console.log(props.vendor)
   return (
-    <h1>VendorCard</h1>
+    <div className="vendorCard">
+      {/* <img src={`${props.vendor.image}`} alt={`${props.vendor.name}`}/> */}
+      <h1 className="vendorName">{props.vendor.name}</h1>
+      <p>{props.vendor.description} <br/> location: {props.vendor.location} <br/> hours: {props.vendor.hours}</p>
+      <button>Select Vendor</button>
+    </div>
   )
 }
 
