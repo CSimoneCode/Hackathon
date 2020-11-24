6929b0bd46e54e7177b4cfc322dd519f7948ee5e
jsans24 <jsans24@gmail.com>
2020-11-23 16:16:12 -0800

    JS2 -  added login and register pages
    Signed-off-by: jsans24 <jsans24@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
       </Switch>
     </div>
   );
