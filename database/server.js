ac0a2eed14ccd7a175f90136d54c02766836804b
kihwan-lee <kihwanlee97@gmail.com>
2020-11-23 18:10:56 -0600

    Controllers and Routes Finished

--- database/server.js
+++ database/server.js
@@ -8,8 +8,15 @@ const app = express();
 const corsOptions = {
   origin : 'https://localhost:3000/'
 }
+// middleware - JSON parsing
+app.use(express.json());
 
+// middle - Routes - Simone! 
+app.use("/api/v1/games", routes.games);
+
+// Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
 
+// Listener
 app.listen(port, () => console.log(`Server running on port ${port}`));
